d5f0735f3bc6aebbcf0a0e3579a38c036bdcc7cb
roz88hub <41391440+roz88hub@users.noreply.github.com>
2018-10-15 14:46:23 -0400

    Initial commit

---
+++ calculate.js
@@ -0,0 +1,20 @@
+var b ="";
+$( document ).ready(function() {
+	var firstInp = document.getElementById('calculatorInput');
+$(".inputElements").on( "click", function() {
+	firstInp.innerHTML+=($(this).text());
+		secInp = firstInp.innerHTML;
+});
+$(".evaluate").on( "click", function() {
+	try {
+		 var finRes  = eval(secInp);
+		 firstInp.innerHTML = finRes;
+		}catch(err) {
+			 firstInp.innerHTML = err.message;
+		 }
+ });
+
+$(".clear").on( "click", function() {
+	$('#calculatorInput').empty();
+});
+});
